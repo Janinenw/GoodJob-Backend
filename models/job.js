a7a55d84a0739e114c05c786be85e27bb089639d
Janinenw <janine.wexler@gmail.com>
2023-05-24 00:24:11 -0400

    updated job schema

--- models/job.js
+++ models/job.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const JobSchema = new mongoose.Schema({
     company: { type: String, required: true },
     position: { type: String, required: true },
-    appStatus: { type: String, required: true },
-    importantDate: { type: String, required: true },
+    appStatus: { type: String, enum:['Sent','Waiting', 'Next Round'], required: true },
+    nextSteps:{type: String},
+    deadline: {type: String},
+    dateApplied:{type: Date, required: true},
+    importantDate: { type: String,},
     notes: { type: String, default: true },
-    finalResult: { type: String, default: true }
+    finalResult: { type: String, enum:['Accepted', 'Rejected'], default: true}
 });
 
 const Job = mongoose.model("Job", JobSchema);
