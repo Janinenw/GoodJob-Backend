e3efdd15f62784c66e750071d28c74b36bd5f1bc
Janinenw <janine.wexler@gmail.com>
2023-05-30 14:06:23 -0400

    token test at jwt.io matches user id

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -30,13 +30,14 @@ const registerUser = asyncHandler(async(req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
         throw new Error ('Invalid user data')
     }
-    // res.json({message: 'Register User'})
+    
 })
 
 const loginUser = asyncHandler(async(req, res) => {
@@ -49,18 +50,26 @@ const loginUser = asyncHandler(async(req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id)
         })
     
     } else { res.status(400)
         throw new Error ('Invalid credentials')
         
     }
-    // res.json({message: 'Login User'})
+   
 })
 
 const getMe = asyncHandler (async(req, res) => {
     res.json({message: 'Here I am! ( user data display)'})
 })
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn:'90d',
+     })
+}
+
 
 module.exports = {
     registerUser, loginUser, getMe
