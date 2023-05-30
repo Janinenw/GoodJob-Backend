4e237cc3b29df7553897bc4a93eda4a4c8c7298a
Janinenw <janine.wexler@gmail.com>
2023-05-30 02:17:19 -0400

    post req. to /users/login successful, get req. to user/me successful

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,6 +2,14 @@ const registerUser = (req, res) => {
     res.json({message: 'Register User'})
 }
 
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'})
+}
+
+const getMe = (req, res) => {
+    res.json({message: 'Here I am! ( user data display)'})
+}
+
 module.exports = {
-    registerUser,
+    registerUser, loginUser, getMe
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser} = require ('../controllers/userCtrl'
+const {registerUser,loginUser, getMe} = require ('../controllers/userCtrl'
 )
 
 router.post('/', registerUser)
+router.post('/login', loginUser)
+router.get('/me', getMe)
 
 module.exports = router
