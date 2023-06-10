fce15d19bc585fecbd766ba56cf5ea48b40243c9
Janinenw <janine.wexler@gmail.com>
2023-06-09 20:27:16 -0400

    schema update, n/a now works

--- models/job.js
+++ models/job.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const JobSchema = new Schema({
   company: { type: String, required: true },
   position: { type: String, required: false },
-  appStatus: { type: String, enum:['Sent','Working On ', 'Next Round'], required: false },
+  appStatus: { type: String, enum: ['Sent','Working On','Next Round'], required: false },
   nextSteps: { type: String, required: false },
   deadline: { type: String, required: false },
   dateApplied: { type: Date, required: false },
   importantDate: { type: String, required: false },
   notes: { type: String, required: false },
-  finalResult: { type: String, enum:['Accepted', 'Rejected', 'N/A, but it will be!'], required: false },
-  user_id: { type: String, required: true } 
+  finalResult: { type: String, enum: ['Accepted', 'Rejected','N/A but it will be!'], required: false },
+  user_id: { type: String, required: true }
 });
 
 const Job = mongoose.model('Job', JobSchema);
--- seed.js
+++ seed.js
@@ -1,65 +1,66 @@
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const Job = require('./models/job');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Job = require('./models/job');
 
-// const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// mongoose.set('debug', true);
-
-
-// mongoose.connect(MONGODB_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.set('debug', true);
 
 
-// mongoose.connection
-//   .on('open', () => console.log('You are connected to MongoDB- seed.js'))
-//   .on('close', () => console.log("You are disconnected from MongoDB-seed.js"))
-//   .on('error', (error) => console.error("MongoDB connection error:-seed.js", error));
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
-// const jobsData = [
-//     {
-//       company: 'FAANG Custom Dental Implants',
-//       position: 'Receptionist',
-//       appStatus: 'Next Round',
-//       nextSteps: 'Interview scheduled',
-//       deadline: '2023-10-31',
-//       dateApplied: new Date(),
-//       importantDate: '2023-06-15 interview',
-//       notes: 'Dentist is Vampire. All interviews must be scheduled at night',
-//       finalResult: 'Accepted',
-//     },
-// //     {
-//       company: 'Bootstrap Farms',
-//       position: 'Potato Manager',
-//       appStatus: 'Sent',
-//       nextSteps: '',
-//       deadline: '2023-06-13',
-//       dateApplied: new Date(),
-//       importantDate: '',
-//       notes: 'Call Gore',
-//       finalResult: 'Rejected',
-//     },
-    
-//   ];
+mongoose.connection
+  .on('open', () => console.log('You are connected to MongoDB- seed.js'))
+  .on('close', () => console.log("You are disconnected from MongoDB-seed.js"))
+  .on('error', (error) => console.error("MongoDB connection error:-seed.js", error));
 
-// const seedJob = async () => {
-//   try {
+
+  const jobsData = [
+    {
+      company: 'FAANG Custom Dental Implants',
+      position: 'Receptionist',
+      appStatus: 'Working On',
+      nextSteps: 'Interview scheduled',
+      deadline: '2023-10-31',
+      dateApplied: new Date(),
+      importantDate: '2023-06-15 interview',
+      notes: 'Dentist is Vampire. All interviews must be scheduled at night',
+      finalResult: 'Accepted',
+      user_id: 'the_user_id_here' 
+    },
+    {
+      company: 'Bootstrap Farms',
+      position: 'Potato Manager',
+      appStatus: 'Sent',
+      nextSteps: '',
+      deadline: '2023-06-13',
+      dateApplied: new Date(),
+      importantDate: '',
+      notes: 'Call Gore',
+      finalResult:'N/A but it will be!',
+      user_id: 'the_user_id_here' 
+    },
+  ];
+
+const seedJob = async () => {
+  try {
    
-//     await Job.insertMany(jobsData);
+    await Job.insertMany(jobsData);
 
-//     console.log('Test jobs seeded successfully.');
-//   } catch (error) {
-//     console.error('Error seeding jobs:', error);
-//   } finally {
+    console.log('Test jobs seeded successfully.');
+  } catch (error) {
+    console.error('Error seeding jobs:', error);
+  } finally {
     
-//     mongoose.disconnect();
-//   }
-// };
+    mongoose.disconnect();
+  }
+};
 
 
-// seedJob();
+seedJob();
 
 
